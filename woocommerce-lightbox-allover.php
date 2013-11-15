de54a676be4afec4ad165cf2d5308c2ddbef4d0f
toscho <info@toscho.de>
2013-11-15 09:17:12 +0100

    Refactor PrettyPhoto plugin.

--- woocommerce-lightbox-allover.php
+++ woocommerce-lightbox-allover.php
@@ -1,49 +1,69 @@
 <?php # -*- coding: utf-8 -*-
 /**
- * Plugin Name: Allover WooCommerce PrettyPhoto Script
- * Description: Use the WooCommerce PrettyPhoto Lightbox for galleries and single images
+ * Plugin Name: Use WooCommerce PrettyPhoto Sverywhere
+ * Description: Use the WooCommerce PrettyPhoto Lightbox for galleries and single images.
  * Plugin URI:  http://marketpress.com/
- * Version:     2013.11.14
+ * Version:     2013.11.15
  * Author:      MarketPress, Birgit Olzem
  * Author URI:  http://marketpress.com/
  * Licence:     GPL 2
  * License URI: http://opensource.org/licenses/GPL-2.0
  */
 
+// Called in woocommerce/woocommerce.php
+add_action( 'woocommerce_loaded', 'marketpress_init_prettyphoto' );
+
+function marketpress_init_prettyphoto() {
+	add_action( 'wp_enqueue_scripts', 'marketpress_enqueue_prettyphoto_scripts' );
+	add_filter( 'wp_get_attachment_link', 'marketpress_add_rel_attribute' );
+}
+
 
 
 /**
- * Check if WooCommerce is active
- **/
-if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-    
-   	add_action( 'wp_enqueue_scripts', 'frontend_scripts_include_lightbox' );
-   	/**
-   	  * @global $woocommerce
-   	  * @wp-hook wp_enqueue_script
-   	  * @wp-hook wp_enqueue_style
-   	  * @return void
-   	  */ 
-	function frontend_scripts_include_lightbox() {
-	  global $woocommerce;
-	 
-	  $suffix      = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
-	  $lightbox_en = get_option( 'woocommerce_enable_lightbox' ) == 'yes' ? true : false;
-	 
-	  if ( $lightbox_en ) {
-	    wp_enqueue_script( 'prettyPhoto', $woocommerce->plugin_url() . '/assets/js/prettyPhoto/jquery.prettyPhoto' . $suffix . '.js', array( 'jquery' ), '3.1.5', true );
-	    wp_enqueue_script( 'prettyPhoto-init', $woocommerce->plugin_url() . '/assets/js/prettyPhoto/jquery.prettyPhoto.init' . $suffix . '.js', array( 'jquery' ), $woocommerce->version, true );
-	    wp_enqueue_style( 'woocommerce_prettyPhoto_css', $woocommerce->plugin_url() . '/assets/css/prettyPhoto.css' );
-	  }
-	}
-	
-	add_filter('wp_get_attachment_link', 'marketpress_add_rel_attribute');
-	/**
-	 * @global $post  
-	 * @return string
-	 */
-	function marketpress_add_rel_attribute($link) {
-	global $post;
-	return str_replace('<a href', '<a rel="prettyPhoto[pp_gal]" href', $link);
-	}
+ * Register JavaScript and CSS files.
+ *
+ * @global  $woocommerce
+ * @wp-hook wp_enqueue_script
+ * @return  void
+ */
+function marketpress_enqueue_prettyphoto_scripts() {
+	global $woocommerce;
+
+	// Lightbox disabled.
+	if ( get_option( 'woocommerce_enable_lightbox' ) !== 'yes' )
+		return;
+
+	$suffix  = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+	$woo_url = $woocommerce->plugin_url();
+
+	wp_enqueue_script(
+		'prettyPhoto',
+		"$woo_url/assets/js/prettyPhoto/jquery.prettyPhoto$suffix.js",
+		array( 'jquery' ),
+		'3.1.5',
+		TRUE
+	);
+	wp_enqueue_script(
+		'prettyPhoto-init',
+		"$woo_url/assets/js/prettyPhoto/jquery.prettyPhoto.init$suffix.js",
+		array( 'jquery' ),
+		$woocommerce->version,
+		TRUE
+	);
+	wp_enqueue_style(
+		'woocommerce_prettyPhoto_css',
+		"$woo_url/assets/css/prettyPhoto.css"
+	);
+}
+
+/**
+ * Change attachment links.
+ *
+ * @wp-hook wp_get_attachment_link
+ * @param   string $link
+ * @return  string
+ */
+function marketpress_add_rel_attribute( $link ) {
+	return str_replace( '<a href', '<a rel="prettyPhoto[pp_gal]" href', $link );
 }
